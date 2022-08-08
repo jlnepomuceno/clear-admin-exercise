2f3f51b42ce7aad751c80dddd9fff6b293476c17
John Louie Nepomuceno <jlouienepomuceno@gmail.com>
2022-08-08 08:51:30 +0800

    Remove console logs

--- src/api/APIRequests.js
+++ src/api/APIRequests.js
@@ -2,22 +2,17 @@ import axios from "axios";
 import CacheData from "../cache/data.json";
 import { API_SERVER_URL } from "../config/config";
 
-
-// Get the list of breeds available under the CatsAPI
-// then use a callback function to return the response
-// to the function call
 export function requestData(callback) {
   callback(CacheData.skus)
-  // axios
-  //   .get(API_SERVER_URL)
-  //   .then((response) => {
-  //     console.log("meron")
-  //     callback(response.skus);
-  //   })
-  //   .catch((error) => {
-  //     if(error.code === "ERR_NETWORK") callback(CacheData.skus);
-  //     console.log(
-  //       `Error in getting data from the database: ${error.code} | ${error.message}`
-  //     );
-  //   });
+  axios
+    .get(API_SERVER_URL)
+    .then((response) => {
+      callback(response.skus);
+    })
+    .catch((error) => {
+      if(error.code === "ERR_NETWORK") callback(CacheData.skus);
+      console.log(
+        `Error in getting data from the database: ${error.code} | ${error.message}`
+      );
+    });
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,7 +13,6 @@ export default function Home() {
   // for the select input box 
   const getData = () => {
     requestData(data => {
-      console.log("data", data);
       setCatalogData(data);
     });
   };
